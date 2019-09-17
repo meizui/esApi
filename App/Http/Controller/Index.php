3bf27cc3b6bea5ae623fee66177fc5fbb0b9ff77
lanrenwf <ssi_lavarel@163.com>
2019-09-17 15:22:42 +0800

    1

--- App/Http/Controller/Index.php
+++ App/Http/Controller/Index.php
@@ -13,18 +13,7 @@ class Index extends Controller
 
         echo 123123;
         $this->writeJson(200, [], 'success');
-        return '/test';
+       
     }
 
-    function test()
-    {
-        $this->response()->write('this is test');
-        return '/test2';//当执行完test方法之后,返回/test2,让框架继续调度/test2方法
-    }
-
-    function test2()
-    {
-        $this->response()->write('this is test2');
-        return true;
-    }
 }
