1d121cfdec90a907457f4e0f96849188232498b9
lanrenwf <ssi_lavarel@163.com>
2019-09-17 15:07:19 +0800

    1

---
+++ App/Http/Controller/Index.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\HttpController;
+
+use EasySwoole\EasySwoole\Trigger;
+use EasySwoole\Http\AbstractInterface\Controller;
+use EasySwoole\Http\Message\Status;
+
+class Index extends Controller
+{
+    function index()
+    {
+        $this->writeJson(200, [], 'success');
+        return '/test';
+    }
+
+    function test()
+    {
+        $this->response()->write('this is test');
+        return '/test2';//当执行完test方法之后,返回/test2,让框架继续调度/test2方法
+    }
+
+    function test2()
+    {
+        $this->response()->write('this is test2');
+        return true;
+    }
+}
