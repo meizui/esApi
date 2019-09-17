4f826dc5ce0dbf3bd716ddb270d32c5241658721
lanrenwf <ssi_lavarel@163.com>
2019-09-17 17:33:08 +0800

    1

--- App/HttpController/Api/Index.php
+++ App/HttpController/Api/Index.php
@@ -4,12 +4,24 @@ namespace App\HttpController\Api;
 
 namespace App\HttpController\Api;
 use App\HttpController\Api\Base;
+use EasySwoole\Component\Di;
 
 class Index extends Base
 {
 
    public function test () {
         return  $this->failed('请求失败');
+   }
+
+   public function getData ()
+   {
+        $db = Di::getInstance()->get('mysql');
+        $data = $db->where('id',1)->getOne('video');
+        if ($data) {
+            return $this->writeOk($data);
+        } else {
+            return $this->failed();
+        }
    }
 
 }
