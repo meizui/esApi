6c780974087d4c5ff9ac90a8d60db06cf4f7f554
lanrenwf <ssi_lavarel@163.com>
2019-10-07 16:29:54 +0800

    1

--- App/HttpController/Api/Upload.php
+++ App/HttpController/Api/Upload.php
@@ -14,8 +14,8 @@ class Upload extends Base
     {
 
         $request = $this->request();
-//        $uploaded_file = $request->getUploadedFile('file');
-        $file = $request->getUploadedFiles('file');
+        $file = $request->getUploadedFile('file');
+//        $file = $request->getUploadedFiles('file');
 
         var_dump($file);
 
