bec9fe741077a33cc3602c7fde68f9e069c97c5e
lanrenwf <ssi_lavarel@163.com>
2019-10-18 16:24:21 +0800

    1

--- App/Lib/Upload/Base.php
+++ App/Lib/Upload/Base.php
@@ -8,7 +8,6 @@ class Base {
     public $request;
     // 上传文件的key的名称
     public $type;
-
     public $size;
 
     public function __construct($request)
@@ -25,19 +24,16 @@ class Base {
         if($this->type != $this->fileType) return false;
         // 检查文件大小
         $upFile = $this->request->getUploadedFile($this->type);
-        $this->size  = $upFile->getSize();
-        $this->mediaType = $upFile->getMediaType();
-
-        var_dump($this->size);
-        var_dump($this->mediaType);
-
-
-
-
-//        $savePath = '/www/esApi';
-//        $save = $savePath . '/' . time() . '.' . $upFile->getClientFilename();
-//        $res = $upFile->moveTo($save);
-
+        $savePath = '/www/esApi';
+        $save = $savePath . '/' . time() . '.' . $upFile->getClientFilename();
+        $res = $upFile->moveTo($save);
+        if ($res) {
+            return [
+                'path'=>$save
+            ];
+        }else {
+            return false;
+        }
 
     }
 
